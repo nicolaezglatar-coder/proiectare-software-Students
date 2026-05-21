08afac3b75103baacf35a72d7214b2d1a5edf10d
nicolae zglatar <nicolae.zglatar@ulbsibiu.ro>
2026-05-22 02:21:37 +0300

    Rezolvare proiect Students laborator 1

---
+++ src/main/java/ro/ulbs/proiectaresoftware/students/Student.java
@@ -0,0 +1,37 @@
+package ro.ulbs.proiectaresoftware.students;
+
+public class Student {
+    private int numarMatricol;
+    private String prenume;
+    private String nume;
+    private String formatieDeStudiu;
+
+    public Student(int numarMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+
+    public int getNumarMatricol() {
+        return numarMatricol;
+    }
+
+    public String getPrenume() {
+        return prenume;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public String getFormatieDeStudiu() {
+        return formatieDeStudiu;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15d %-15s %-15s %-15s",
+                numarMatricol, prenume, nume, formatieDeStudiu);
+    }
+}
